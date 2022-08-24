81f3141583013ba28fbf8b6bfcd70719cb9ff5b4
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-08-24 00:39:03 -0400

    Add two numbers

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-const http = require('http');
+const addNums = require("./addNums");
 
-const hostname = '127.0.0.1';
-const port = 4000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const sum = addNums(2, 2);
+console.log(sum);
